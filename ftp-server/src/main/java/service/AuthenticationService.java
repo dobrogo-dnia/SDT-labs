042e80de3564ddcdf5ef73253a257377588e41c7
dobrogo-dnia <evhenii.voroniuk@gmail.com>
2024-11-20 20:48:35 +0200

    debugged services

--- ftp-server/src/main/java/service/AuthenticationService.java
+++ ftp-server/src/main/java/service/AuthenticationService.java
@@ -1,33 +1,23 @@
 package service;
 
+import logger.Logger;
 import model.Session;
 import model.User;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class AuthenticationService {
-    private final UserService userService;
-    private final SessionService sessionService;
+    private final UserService userService = UserService.getUserService();
+    private final SessionService sessionService = SessionService.getSessionService();
+    private static AuthenticationService authenticationService;
 
-    public AuthenticationService(UserService userService, SessionService sessionService) {
-        this.userService = userService;
-        this.sessionService = sessionService;
-    }
+    private AuthenticationService() { }
 
-    public User authenticate(String username, String password) {
-        User currentUser = userService.getByUsername(username);
-        if (currentUser != null && currentUser.getPassword().equals(password)) {
-            Session newSession = new Session(currentUser);
-            sessionService.createSession(newSession);
-            return currentUser;
-        }
-        return null;
-    }
-
-    public void signOut(String username) {
-        UserService userService = new UserService();
-        User user = userService.getByUsername(username);
-        Session activeSession = sessionService.getActiveSessionForUser(user.getUserId());
-        if (activeSession != null)
-            sessionService.modifySessionStatus(activeSession.getSessionId());
+    public static AuthenticationService getAuthenticationService() {
+        if(authenticationService == null)
+            authenticationService = new AuthenticationService();
+        return authenticationService;
     }
 
     public boolean userExists(String username) {
@@ -35,4 +25,30 @@ public class AuthenticationService {
         return user != null;
     }
 
+    public boolean userAllowedToHaveNewSession(String username) {
+        User user = userService.getByUsername(username);
+        if(user.getIsAdmin())
+            return true;
+
+        return sessionService.getActiveSessionForUser(user.getUserId()) == null;
+    }
+
+    public User authenticate(String username, String password, Logger logger, Socket clientSocket) throws IOException {
+        User currentUser = userService.getByUsername(username);
+        if(currentUser != null && currentUser.getPassword().equals(password)){
+            if (sessionService.getActiveSessionForUser(currentUser.getUserId()) == null){
+                Session newSession = new Session(currentUser);
+                logger.writeLogInEventToFile(clientSocket.getInetAddress().getHostAddress(), currentUser.getUsername());
+            }
+            return currentUser;
+        }
+        return null;
+    }
+
+    public void signOut(String username) {
+        User user = userService.getByUsername(username);
+        Session activeSession = sessionService.getActiveSessionForUser(user.getUserId());
+        if(activeSession != null)
+            sessionService.modifySessionStatus(activeSession.getSessionId());
+    }
 }
